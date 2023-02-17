822d5221912e8d99aaaba91e9eee59f71787adc7
Devansh Malviya <devansh18@Devanshs-MacBook-Air.local>
2023-02-17 22:48:02 +0530

    inital commit

---
+++ src/utils/api.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const BASE_URL = "https://youtube138.p.rapidapi.com";
+
+const options = {
+    params: { hl: "en", gl: "US" },
+    headers: {
+        "X-RapidAPI-Key":"4af08f5096msh8eba9dd4492c17ap1450e8jsn621168eee13e",
+        "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
+    },
+};
+
+export const fetchDataFromApi = async (url) => {
+    const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+    return data;
+};
